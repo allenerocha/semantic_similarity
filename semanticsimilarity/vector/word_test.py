f7fec6ab0b52026d9b6a98b990f86b8acc68447f
allen <allenerocha@pm.me>
2020-03-24 14:52:48 -0500

    Testing the magnitude when adding words to the vector

--- semanticsimilarity/vector/word_test.py
+++ semanticsimilarity/vector/word_test.py
@@ -1,5 +1,6 @@
 import unittest
 import word
+import math
 
 
 class TestWord(unittest.TestCase):
@@ -85,5 +86,32 @@ class TestWord(unittest.TestCase):
         word_test.append("am")
         self.assertEqual(len(word_test.descriptor_components), 2)
 
+
+    def test_magnitude(self):
+        word_test = word.Word("man")
+
+        # test with no components
+        with self.assertRaises(IndexError):
+            word_test.magnitude
+
+        # test with 1 component with a value of 3
+        word_test.append("i")
+        word_test.append("i")
+        word_test.append("i")
+
+        self.assertAlmostEqual(word_test.magnitude, 3.0)
+
+        # test with 2 components both with values of 3
+        word_test.append("am")
+        word_test.append("am")
+        word_test.append("am")
+
+        self.assertAlmostEqual(word_test.magnitude, math.sqrt(18.0))
+
+        # test with 3 components with a total value of 22
+        word_test.append("a")
+        word_test.append("a")
+
+        self.assertAlmostEqual(word_test.magnitude, math.sqrt(22.0))
 
 
