8de20ace5f1f433e90c21a6a22c9ad730aacff14
allen <allenerocha@pm.me>
2020-03-24 17:34:26 -0500

    Added doc string and checking if path exists

--- semanticsimilarity/utils/corpushandle.py
+++ semanticsimilarity/utils/corpushandle.py
@@ -3,10 +3,23 @@ import os
 
 
 def open_corpus(path: str):
+    """
+    This function opens the passed corpus path
+    :param path: Path of file(s):
+        If the path given ends in a directory, it will iterate over all files in that directory
+        Otherwise it will only work with that file
+    """
+    # checks if the path is a string
     if not isinstance(path, str):
         raise TypeError(f"Expecting path as a string, argument given was of type {type(path)}")
 
+    # checks for empty string
     if len(path) < 1:
         raise ValueError(f"Error handling the argument passed, was given {path}")
+
+    # check if path is not file or directory
+    if not os.path.isdir(path) or not os.path.isfile(path):
+        raise FileNotFoundError(f'Error looking for file or path. The given {path} was not found.')
+
 
 
