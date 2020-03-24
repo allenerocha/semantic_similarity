46dcb711889cb3ded977c2f962c91ee261397f0e
allen <allenerocha@pm.me>
2020-03-24 14:31:35 -0500

    testing adding components to a vector

--- semanticsimilarity/vector/word_test.py
+++ semanticsimilarity/vector/word_test.py
@@ -37,3 +37,53 @@ class TestWord(unittest.TestCase):
             word.Word("")
 
 
+    def test_adding_components(self):
+        # add new component
+        word_test = word.Word("man")
+        self.assertEqual(len(word_test.descriptor_components), 0)
+
+        # test with integer
+        with self.assertRaises(TypeError):
+            word_test.append(1)
+
+        # test with float
+        with self.assertRaises(TypeError):
+            word_test.append(1.0)
+
+        # test with object
+        with self.assertRaises(TypeError):
+            word_test.append(object)
+
+        # test with dict
+        with self.assertRaises(TypeError):
+            word_test.append({"test": 123})
+
+        # test with list
+        with self.assertRaises(TypeError):
+            word_test.append([1])
+
+        # test with tuple
+        with self.assertRaises(TypeError):
+            word_test.append((1, 2))
+
+        # test with empty string
+        with self.assertRaises(ValueError):
+            word_test.append("")
+
+        # increment existing component
+        word_test.append("i")
+        self.assertEqual(len(word_test.descriptor_components), 1)
+
+        word_test.append("i")
+        self.assertEqual(len(word_test.descriptor_components), 1)
+
+        # add new component
+        word_test.append("am")
+        self.assertEqual(len(word_test.descriptor_components), 2)
+
+        # increment existing component
+        word_test.append("am")
+        self.assertEqual(len(word_test.descriptor_components), 2)
+
+
+
