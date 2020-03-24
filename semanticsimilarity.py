24cf02500b6e2d3663d6f48cf6bfb179fdfb91fa
allen <allenerocha@pm.me>
2020-03-24 13:36:16 -0500

    Gets absolute path of directory

--- semanticsimilarity.py
+++ semanticsimilarity.py
@@ -2,9 +2,12 @@
 # encoding: utf-8
 
 import os
+import sys
 
 
 if __name__ == "__main__":
+    # variable to keep track of the absolute path of this file
+    current_path = os.path.dirname(os.path.abspath(__file__))
     # check if the corpus directory exists
     if not os.path.isdir('corpus'):
         raise FileNotFoundError("The corpus directory must exist with text files in order to run this application!")
