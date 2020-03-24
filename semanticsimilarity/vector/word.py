d7f8a24a0a7cc091cbc9c433804a8a466c2c6165
allen <allenerocha@pm.me>
2020-03-24 13:37:01 -0500

    Initial commit

---
+++ semanticsimilarity/vector/word.py
@@ -0,0 +1,48 @@
+"""This module is the descriptor vector and contains all relevant functions"""
+import math
+
+
+class Word:
+    def __init__(self, word: str) -> None:
+        """
+        This function assigns self.word to the passed word argument
+        :param word: string to assign to self.word
+        """
+        if not isinstance(word, str):
+            raise TypeError(f"Expected value to be str type, got {type(word)}.")
+
+        self.word = word
+
+        self.vector = dict()
+        self.vector[self.word] = dict()
+
+
+    def append(self, word: str):
+        if word in self.descriptor_components:
+            self.vector[self.word][word] += 1
+        else:
+            self.vector[self.word][word] = 1
+
+
+    @property
+    def descriptor(self):
+        """This function returns the entire descriptor vector"""
+        return self.vector
+
+
+    @property
+    def descriptor_components(self):
+        """This function returns the components of this descriptor vector"""
+        return self.vector[self.word]
+
+
+    @property
+    def magnitude(self):
+        """Calculates the magnitude of this vector"""
+        if len(self.descriptor_components) < 1:
+            raise IndexError(f"{self.word} must have at least one component in order to calculate its magnitude.")
+
+        squares = [value * value for value in self.descriptor_components.values()]
+        return math.sqrt(sum(squares))
+
+
