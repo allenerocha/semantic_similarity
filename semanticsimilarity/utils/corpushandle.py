fcf1af11bc992809a9de62d3e77b54402788e63a
allen <allenerocha@pm.me>
2020-03-24 15:41:25 -0500

    Path type and value checking

---
+++ semanticsimilarity/utils/corpushandle.py
@@ -0,0 +1,11 @@
+import os
+
+
+def open_corpus(path: str):
+    if not isinstance(path, str):
+        raise TypeError(f"Expecting path as a string, argument given was of type {type(path)}")
+
+    if len(path) < 1:
+        raise ValueError(f"Error handling the argument passed, was given {path}")
+
+
