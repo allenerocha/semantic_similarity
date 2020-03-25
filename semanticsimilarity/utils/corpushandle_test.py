89b32d351ee8348972dfebfd5163479b729dfcef
allen <allenerocha@pm.me>
2020-03-24 19:33:46 -0500

    Added testing for entering a path

---
+++ semanticsimilarity/utils/corpushandle_test.py
@@ -0,0 +1,42 @@
+import unittest
+import corpushandle
+
+
+class TestWord(unittest.TestCase):
+    def corpus_test(self):
+        # check for int
+        with self.assertRaises(TypeError):
+            corpushandle.corpus(1)
+
+        # check for float
+        with self.assertRaises(TypeError):
+            corpushandle.corpus(1.0)
+
+        # check for object
+        with self.assertRaises(TypeError):
+            corpushandle.corpus(object)
+
+        # check for dict
+        with self.assertRaises(TypeError):
+            corpushandle.corpus({"test": 123})
+
+        # check for list
+        with self.assertRaises(TypeError):
+            corpushandle.corpus([1, 2, 3])
+
+        # check for tuple
+        with self.assertRaises(TypeError):
+            corpushandle.corpus((1, 2, 3))
+
+        # check for empty string
+        with self.assertRaises(TypeError):
+            corpushandle.corpus("")
+
+        # check if file exists
+        with self.assertRaises(FileNotFoundError):
+            corpushandle.corpus("notafile.py")
+
+        # check if file exists
+        with self.assertRaises(FileNotFoundError):
+            corpushandle.corpus("notadir/")
+
